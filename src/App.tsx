8d1921aabc8c3b87733ae7313db95d88c3f82f90
Sarah Harrington <sarah@sarahdoes.tech>
2023-08-20 00:12:23 -0500

    added confetti

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react'
 import { Game } from './components/Game';
+import {
+  useWindowSize,
+} from '@react-hook/window-size'
+import Confetti from 'react-confetti'
 
 function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
+  const [width, height] = useWindowSize()
+  const [isFalling, setIsFalling] = useState(true)
+    return (
+      <div className="App">
+        {isFalling &&         
+          <Confetti
+            width={width}
+            height={height}
+          />
+        }
+
+        <Game />
+      </div>
   );
 }
 
